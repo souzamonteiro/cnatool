53415a3ac7642d906fd11986259139e21e71a4bb
Roberto Luiz Souza Monteiro <roberto@souzamonteiro.com>
2020-10-12 15:07:54 -0300

    Index.html file modified to use the compiled version of the CNA library (cna.js)

--- build.sh
+++ build.sh
@@ -2,4 +2,9 @@
 
 rm -rf doc/*
 
+chmod 755 bin/*
+
+bin/maiascript.js -c -o build/cna.js ./maia/cna/cna.maia
+cp build/cna.js js/
+
 jsdoc -c ./jsdoc.json -d ./doc ./maia/cna/package.json ./maia/cna
