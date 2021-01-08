9dd3f06a6d0e741f2dd947808a9d0849541b6a96
Roberto Luiz Souza Monteiro <roberto@souzamonteiro.com>
2021-01-08 18:17:42 -0200

    Bug fix in CNA library compilation

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@
 rm -rf build/*
 rm -rf docs/*
 
-bin/maiascript.js -c -o build/cna.js ./maia/cna/cna.maia
+#bin/maiascript.js -c -o build/cna.js ./maia/cna/cna.maia
+cp ./maia/cna/cna.js build/
 cp build/cna.js src/
 cp build/cna.js js/
 
